819c934edb364e47d2bc1644ea0105c5826746c5
DavidWood2001 <davidwood1301@gmail.com>
2020-09-30 00:16:45 +0100

    Copied the starter code into main.java to begin the exercise

--- Session 1/Exercise 1/src/com/Main.java
+++ Session 1/Exercise 1/src/com/Main.java
@@ -3,6 +3,17 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    /* DATA */
+        String userName = "bob";
+        String password = "pass";
+
+        /* INSTRUCTIONS */
+        System.out.println("Checking password");
+        if (password == "pass" && userName == "bob"){
+            System.out.println("Logging in " + userName);
+        }
+        else {
+            System.out.println("Incorrect password or user name");
+        }
     }
 }
