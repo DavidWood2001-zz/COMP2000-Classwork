1429ce9e7d94713c70e20a0c75c99e7f0ec768fd
DavidWood2001 <davidwood1301@gmail.com>
2020-09-30 10:37:37 +0100

    Copied the code from the second encapsulation exercise into main

--- Session 1/Exercise 1/src/com/Main.java
+++ Session 1/Exercise 1/src/com/Main.java
@@ -9,6 +9,4 @@ public class Main {
         /* INSTRUCTIONS */
         newUser.checkUserInfo();
     }
-
-
 }
--- Session 1/Exercise 2/src/com/Main.java
+++ Session 1/Exercise 2/src/com/Main.java
@@ -3,6 +3,36 @@ package com;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /* DATA */
+        String message = "Having fun in the sun!";
+
+        /* INSTRUCTIONS */
+        System.out.println(" **Post to Social Media App");
+
+        /* DATA */
+        String photo = "Picture of me on the beach";
+
+        /* INSTRUCTIONS */
+        if (message.equals("")) {
+            System.out.println("Please enter a message");
+        } else {
+            System.out.println("Message: " + message);
+        }
+
+        /* INSTRUCTIONS */
+        if (!photo.equals("")) {
+            System.out.println("Photo: " + photo);
+        }
+
+        /* DATA */
+        String tags = "#sun,#beach,#seaside";
+
+        /* INSTRUCTIONS */
+        if (!tags.equals("")) {
+            System.out.println("Tags: " + tags);
+        }
+
+        /* INSTRUCTIONS */
+        System.out.println("Posting to Social Media");
     }
 }
